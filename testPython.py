3d4dd1200d29b4f081642d9e78da1addd6df67f7
nodar-students <157397525+nodar-students@users.noreply.github.com>
2024-01-24 22:09:18 +0400

    Create testPython.py

---
+++ testPython.py
@@ -0,0 +1,42 @@
+# print("Hello","World","Nodari")
+# ====================================================
+# input('What is "your" name? ')
+# ====================================================
+# userName = input("what is your name? ")
+
+# print(int(userName) + 25)
+# ====================================================
+
+# firstNum = float(input("Give me first number: "))
+# secondNum = float(input("Give me second number: "))
+
+# print(firstNum + secondNum)
+# print(firstNum - secondNum)
+# print(firstNum * secondNum)
+# print(firstNum / secondNum)
+# print(firstNum **secondNum)
+# print(firstNum % secondNum)
+# print(firstNum **secondNum)
+
+# ====================================================
+# max_height = 2.0
+# name = input("What is your name? ")
+# height = input("What is your height? ")
+# fevorit_color = input("What is favorite color? ")
+# check_height = max_height > float(height)
+
+# print("Hello, " + name + "!")
+# print("Your height is less than 2.0 meters: " + str(check_height))
+# print("Your favorite color is " + fevorit_color)
+
+# ====================================================
+# age = int(input("What is your age? "))
+# weight = int(input("What is your weight? "))
+# city = input("Wherer are you living? ")
+
+# # result = "You are {} years old, weigh {} kilograms, and live in {}.".format(age,weight,city)
+
+# print("You are {} years old, weigh {} kilograms, and live in {}.".format(age,weight,city))
+# print(f"You are {age:.4f} years old, \nweigh {weight} kilograms, and live in {city}.")
+
+# ====================================================
